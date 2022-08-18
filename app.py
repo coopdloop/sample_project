8c8efbb240cd3b7f9a5216c52736b788a3d3905f
cooper.wallace <cooper.wallace@ankura.com>
2022-08-18 12:57:34 -0500

    added two functions

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ firstValue = int(sys.argv[1])
 secondValue = int(sys.argv[2])
 #print(secondValue)
 thirdValue = sys.argv[3]
+
+
+def add(value1,value2):
+    answer=value1+value2
+    return answer
+
+def subtr(value1,value2):
+    answer=value1-value2
+    return answer    
